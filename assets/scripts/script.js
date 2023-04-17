7a72eb9fd0728b7d65402d5cb7b49b338186245b
Jose Alzuri <jalzu1007@gmail.com>
2023-04-16 22:52:48 -0400

    Added Loop with random generator and connected the array to password for a return

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Arrays for password to be used in the generator for the user
+// Arrays for the characters password to be used in the generator for the user
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z" ];
 var numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "/", "?"];
 
+//Arrays for the input value from the user
 var userOptions = [];
 var userPassword = [];
 
@@ -17,40 +18,58 @@ function writePassword() {
 
   passwordText.value = password;
 } 
-// Generate password inputs for the user
+
+//Generate password inputs for the user
 function generatePassword() {
 var passwordLength = prompt("How many characters would you like your password to contain?");
-  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+  if (passwordLength === null) {
+    return;
+} 
+
+  else if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("Please enter a valid number length between 8 and 128 characters.");
     generatePassword();
   }
 
-  else{
-    lowerCase = confirm("Click OK to confirm including lower case characters.");
-    upperCase = confirm("Click OK to confirm including upper case letters.");
-    numberCharacters = confirm("Click OK to confirm including numeric characters.");
-    symbolCharacters = confirm("Click OK to confirm including special characters.");
+  else {
+    includeLowercase = confirm("Click OK to confirm including lower case characters.");
+    includeUppercase = confirm("Click OK to confirm including upper case letters.");
+    includeNumeric = confirm("Click OK to confirm including numeric characters.");
+    includeSpecial = confirm("Click OK to confirm including special characters.");
   }
-
-  //With the user input determine which Arrays it applies too, connecting both string Arrays
-  if (lowercase === true) {
+    
+//With the user input determine which Arrays it applies too, connecting the string Arrays to userOptions
+  if (includeLowercase === true) {
     userOptions =  userOptions.concat(lowerCaseLetters);
   }
 
-  if (upperCase === true) {
+  if (includeUppercase === true) {
     userOptions = userOptions.concat(upperCaseLetters);
   }
 
-  if (numberCharacters === true) {
+  if (includeNumeric === true) {
     userOptions = userOptions.concat(numericCharacters);
   }
 
-  if (symbolCharacters === true) {
+  if (includeSpecial === true) {
     userOptions = userOptions.concat(specialCharacters);
   }
-}
+
+//Added if statement if any characters weren't selected, takes back to generatePassword()
+  if (userOptions.length === 0) {
+    alert("Please confirm at least one type of character to include in your password.");
+    generatePassword();
+  }
 
 //Loop through the arrays to create a userPassword to match the passwordLength given by the user
+  for (var i = 0; i < passwordLength; i++) {
+    var userIndex = userOptions[Math.floor(Math.random() * userOptions.length)];
+    userPassword.push(userIndex);
+}
+
+    var password = userPassword.join("") 
+    return password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
